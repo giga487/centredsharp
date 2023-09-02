910427a0d98af6a1785df606bfcc9241be5835b0
giga487 <giga487@hotmail.com>
2023-09-02 17:23:39 +0200

    inserito fiddler

---
+++ UoSeaSite/Utils/CentredConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace UoSeaSite
+{
+    public class CentredConfiguration
+    {
+        public string? this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Path { get; set; } = Directory.GetCurrentDirectory();
+        public string Output { get; set; } = Directory.GetCurrentDirectory();
+        public int Port { get; set; } = 10000;
+        public string Host { get; set; } = "localhost";
+    }
+}
